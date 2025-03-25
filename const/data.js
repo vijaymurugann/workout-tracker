8de845ed40cfc1cfba64a9cb7840c7ab73742b3a
Vijay M <vijaymurugan177@gmail.com>
2025-03-26 00:07:13 +0530

    DATA update

--- const/data.js
+++ const/data.js
@@ -9,32 +9,56 @@ export const initialWorkoutData = {
       exercises: [
         {
           id: 101,
-          name: "Flat bench press",
+          name: "Push ups",
           completed: false,
           sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
             { reps: "", weight: "" },
           ],
         },
         {
           id: 102,
-          name: "Incline dumbbell press",
+          name: "Flat bench press",
           completed: false,
           sets: [
             { reps: "", weight: "" },
-          
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
           ],
         },
         {
           id: 103,
-          name: "High to low cable flies",
+          name: "Incline dumbbell press",
           completed: false,
           sets: [
-            { reps: "0", weight: "0" },
-
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
           ],
         },
         {
           id: 104,
+          name: "High to low cable flies",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 105,
+          name: "Weighted dips",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 106,
           name: "Pec fly",
           completed: false,
           sets: [
@@ -58,7 +82,8 @@ export const initialWorkoutData = {
           completed: false,
           sets: [
             { reps: "", weight: "" },
-   
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
           ],
         },
         {
@@ -108,17 +133,10 @@ export const initialWorkoutData = {
       name: "Wednesday",
       shortName: "WED",
       date: "March 24",
-      type: "rest",
-    },
-    {
-      id: 4,
-      name: "Thursday",
-      shortName: "THU",
-      date: "March 25",
       focus: "Legs",
       exercises: [
         {
-          id: 401,
+          id: 301,
           name: "Squats",
           completed: false,
           sets: [
@@ -128,8 +146,67 @@ export const initialWorkoutData = {
           ],
         },
         {
+          id: 302,
+          name: "Leg extension",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 303,
+          name: "Hamstring curls",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 304,
+          name: "RDL",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 305,
+          name: "Calves",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Thursday",
+      shortName: "THU",
+      date: "March 25",
+      focus: "Arms",
+      exercises: [
+        {
+          id: 401,
+          name: "Close grip bench press",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
           id: 402,
-          name: "Leg press",
+          name: "Tricep push down",
           completed: false,
           sets: [
             { reps: "", weight: "" },
@@ -139,7 +216,77 @@ export const initialWorkoutData = {
         },
         {
           id: 403,
-          name: "Leg extensions",
+          name: "Overhead extension",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 404,
+          name: "Dips",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 405,
+          name: "Close grip push ups",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 406,
+          name: "Incline dumbbell curl",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 407,
+          name: "Preacher curl",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 408,
+          name: "Hammer curl",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 409,
+          name: "Cable curl",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 410,
+          name: "Forearms",
           completed: false,
           sets: [
             { reps: "", weight: "" },
@@ -154,11 +301,11 @@ export const initialWorkoutData = {
       name: "Friday",
       shortName: "FRI",
       date: "March 26",
-      focus: "Arms",
+      focus: "Shoulders",
       exercises: [
         {
           id: 501,
-          name: "Bicep curls",
+          name: "Dumbbell press",
           completed: false,
           sets: [
             { reps: "", weight: "" },
@@ -168,7 +315,37 @@ export const initialWorkoutData = {
         },
         {
           id: 502,
-          name: "Tricep extensions",
+          name: "Cable lateral raise",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 503,
+          name: "Arnold press",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 504,
+          name: "Rear delt flies",
+          completed: false,
+          sets: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+        {
+          id: 505,
+          name: "Shrugs",
           completed: false,
           sets: [
             { reps: "", weight: "" },
